8157ed252c3b2473a48df36781baa641c34d8440
JeanBiza <jampybizama21@gmail.com>
2026-06-18 11:34:49 -0400

    feat: function extract more games instead of one

--- main.py
+++ main.py
@@ -4,23 +4,43 @@ from dotenv import load_dotenv
 load_dotenv()
 
 PGN_FILE_PATH = os.getenv("PGN_FILE_PATH")
+MAX_GAMES_TO_PROCESS = 5
 
-def test_read_game():
+def extract_games():
+    games_processed = 0
     with open(PGN_FILE_PATH, "r", encoding="utf-8") as pgn:
-        first_game = chess.read_game(pgn)
+        while True:
+            game = chess.read_game(pgn)
 
-        if first_game is None:
-            print("No se encontraron partidas.")
-            return
-        
-        white_player = first_game.headers.get("White", "Unknown")
-        black_player = first_game.headers.get("Black", "Unknown")
-        result = first_game.headers.get("Result", '*')
+            if game is None:
+                break
 
-        print(f"Partida leida con exito!")
-        print(f"White: {white_player}")
-        print(f"Black: {black_player}")
-        print(f"Resultado: {result}")
+            if games_processed >= MAX_GAMES_TO_PROCESS:
+                break
+
+            game_result = game.headers.get("Result", "*")
+
+            if game_result not in ["1-0", "0-1", "1/2-1/2"]:
+                continue
+
+            print(f"\n--- Processing Game {games_processed+1} ---")
+            print(f"Target Label: {game_result}")
+
+            board = game.board()
+
+            move_count = 0
+            for move in game.mainline_moves():
+                board.push(move)
+                move_count += 1
+            
+            print(f"Total half-moves (ply) played: {move_count}")
+
+            print("Final board state:")
+            print(board)
+
+            games_processed += 1
+    
+    print(f"\nFinished processing {games_processed} games successfully.")
 
 if __name__ == "__main__":
-    test_read_game()
+    extract_games()
